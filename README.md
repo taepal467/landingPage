5db8d17fd0272098672e9aef4f2b7c5dcb61e12e
Chantae Palacio <chantae.palacio@gmail.com>
2022-06-21 19:53:15 -0700

    Completed the desktop version of the web page

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ The Odin Project:  Landing Page
 - [Coney Island Listing](https://twitter.com/southpark/status/1495439259628621831) 
 - [Clyde's House](https://twitter.com/southpark/status/1494796264554307585)
 - [CTA Quote](https://johnfulton85.medium.com/the-bar-for-realtors-is-so-low-tim-dillon-and-south-park-take-shots-at-real-estate-sales-people-b556636554b6) - I used this image to create the call to action section
+- [Slideshow for Images](https://www.w3schools.com/howto/howto_js_slideshow.asp) 
+- [Animation](https://animate.style/) - Animation I used for the Call to Action section
---
+++ script.js
@@ -0,0 +1,25 @@
+const plusSlides = (n, no) => {
+    showSlides(slideIndex[no] += n, no);
+}
+
+const showSlides = (n, no) => {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
+
+let slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+
+
+
+
